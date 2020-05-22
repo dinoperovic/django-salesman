f03bdae1a2d5b09647dd7f970da5650a3e2ca363
Dino Perovic <dino.perovic@gmail.com>
2020-05-22 21:37:15 +0200

    Refactor settings

--- salesman/conf.py
+++ salesman/conf.py
@@ -14,6 +14,17 @@ class DefaultSettings:
 
         raise ImproperlyConfigured(message)
 
+    def _callable(self, value):
+        from django.utils.module_loading import import_string
+
+        try:
+            cb = import_string(value)
+        except ImportError as e:
+            self._error(e)
+        if not callable(cb):
+            self._error(f"Specified `{cb}` is not callable.")
+        return cb
+
     @property
     def SALESMAN_PRODUCT_TYPES(self) -> dict:
         """
@@ -22,24 +33,27 @@ class DefaultSettings:
         formated as ``'app_label.Model': 'path.to.Serializer'``.
         """
         from django.apps import apps
-        from django.utils.module_loading import import_string
 
         product_types = self._setting('SALESMAN_PRODUCT_TYPES', {})
         ret = {}
+
         for key, value in product_types.items():
             try:
                 app_label, model_name = key.split('.')
             except ValueError:
                 self._error(f"Invalid Key `{key}`, format as `app_label.Model`.")
+
             try:
                 model = apps.get_model(app_label, model_name)
-                if value:
-                    ret[key] = import_string(value)
-            except (LookupError, ImportError) as e:
+            except LookupError as e:
                 self._error(e)
+
+            ret[key] = self._callable(value)
+
             for attr in ['name', 'code']:
                 if not hasattr(model, attr):
                     self._error(f"Product type `{key}` must define `{attr}` attribute.")
+
             if not hasattr(model, 'get_price'):
                 self._error(f"Product type `{key}` must implement `get_price` method.")
         return ret
@@ -51,47 +65,27 @@ class DefaultSettings:
         Modifiers must extend ``salesman.basket.modifiers.BasketModifier`` class.
         and define a unique ``identifier`` attribute.
         """
-        from django.utils.module_loading import import_string
         from salesman.basket.modifiers import BasketModifier
 
         basket_modifiers = self._setting('SALESMAN_BASKET_MODIFIERS', [])
         ret, identifiers = [], []
+
         for value in basket_modifiers:
-            try:
-                modifier = import_string(value)
-            except ImportError as e:
-                self._error(e)
+            modifier = self._callable(value)
+            identifier = getattr(modifier, 'identifier', None)
+
             if not issubclass(modifier, BasketModifier):
                 self._error(f"Modifer `{modifier}` must subclass `{BasketModifier}`.")
-            identifier = getattr(modifier, 'identifier', None)
+
             if not identifier:
                 self._error(f"Modifier `{modifier}` must define a unique `idetifier`.")
+
             if identifier in identifiers:
                 self._error(f"Modifier `{identifier}` appears more than once.")
+
             identifiers.append(identifier)
             ret.append(modifier)
         return ret
-
-    @property
-    def SALESMAN_EXTRA_VALIDATOR(self) -> callable:
-        """
-        A dotted path to extra validator function. Function should accept a dict
-        value and return a validated version. Also recieves a ``context`` dictionary
-        with additional validator context data like ``request``, a ``basket`` object
-        and ``basket_item`` in case validation is for bakset item.
-        """
-        from django.utils.module_loading import import_string
-
-        value = self._setting(
-            'SALESMAN_EXTRA_VALIDATOR', 'salesman.basket.utils.validate_extra'
-        )
-        try:
-            validator = import_string(value)
-        except ImportError as e:
-            self._error(e)
-        if not callable(validator):
-            self._error(f"Specified extra validator `{validator}` is not callable.")
-        return validator
 
     @property
     def SALESMAN_PAYMENT_METHODS(self) -> list:
@@ -100,49 +94,30 @@ class DefaultSettings:
         Payments must extend ``salesman.checkout.payment.PaymentMethod`` class
         and define a unique ``identifier`` attribute.
         """
-        from django.utils.module_loading import import_string
         from salesman.checkout.payment import PaymentMethod
 
         payment_methods = self._setting('SALESMAN_PAYMENT_METHODS', [])
         ret, identifiers = [], []
+
         for value in payment_methods:
-            try:
-                payment = import_string(value)
-            except ImportError as e:
-                self._error(e)
+            payment = self._callable(value)
+            identifier = getattr(payment, 'identifier', None)
+
             if not issubclass(payment, PaymentMethod):
                 self._error(f"Payment `{payment}` must subclass `{PaymentMethod}`.")
+
             if not getattr(payment, 'label', None):
                 self._error(f"Payment `{payment}` must define a `label`.")
-            identifier = getattr(payment, 'identifier', None)
+
             if not identifier:
                 self._error(f"Payment `{payment}` must define a unique `identifier`.")
+
             if identifier in identifiers:
                 self._error(f"Payment `{identifier}` appears more than once.")
+
             identifiers.append(identifier)
             ret.append(payment)
         return ret
-
-    @property
-    def SALESMAN_ADDRESS_VALIDATOR(self) -> callable:
-        """
-        A dotted path to address validator function. Function should accept a string
-        value and return a validated version. Also recieves a ``context`` dictionary
-        with additional validator context data like ``request``, a ``basket`` object
-        and ``address`` type (set to either *shipping* or *billing*).
-        """
-        from django.utils.module_loading import import_string
-
-        value = self._setting(
-            'SALESMAN_ADDRESS_VALIDATOR', 'salesman.checkout.utils.validate_address'
-        )
-        try:
-            validator = import_string(value)
-        except ImportError as e:
-            self._error(e)
-        if not callable(validator):
-            self._error(f"Specified address validator `{validator}` is not callable.")
-        return validator
 
     @property
     def SALESMAN_ORDER_STATUS(self) -> type:
@@ -154,18 +129,15 @@ class DefaultSettings:
         returns a dict of statuses with their transitions and ``validate_transition``
         method to validate status transitions.
         """
-        from django.utils.module_loading import import_string
         from salesman.orders.status import BaseOrderStatus
 
-        value = self._setting(
-            'SALESMAN_ORDER_STATUS', 'salesman.orders.status.OrderStatus'
-        )
-        try:
-            status = import_string(value)
-        except ImportError as e:
-            self._error(e)
+        default = 'salesman.orders.status.OrderStatus'
+        value = self._setting('SALESMAN_ORDER_STATUS', default)
+        status = self._callable(value)
+
         if not issubclass(status, BaseOrderStatus):
             self._error(f"Status `{status}` must subclass `{BaseOrderStatus}`.")
+
         required = ['NEW', 'CREATED', 'COMPLETED', 'REFUNDED']
         for item in required:
             if item not in status.names or status[item].value != item:
@@ -182,18 +154,9 @@ class DefaultSettings:
         Function should accept a django request object as param: ``request``.
         Value returned from the function will be slugified.
         """
-        from django.utils.module_loading import import_string
-
-        value = self._setting(
-            'SALESMAN_ORDER_REFERENCE_GENERATOR', 'salesman.orders.utils.generate_ref',
-        )
-        try:
-            generator = import_string(value)
-        except ImportError as e:
-            self._error(e)
-        if not callable(generator):
-            self._error(f"Specified reference generator `{generator}` is not callable.")
-        return generator
+        default = 'salesman.orders.utils.generate_ref'
+        value = self._setting('SALESMAN_ORDER_REFERENCE_GENERATOR', default)
+        return self._callable(value)
 
     @property
     def SALESMAN_PRICE_FORMATTER(self) -> callable:
@@ -203,18 +166,33 @@ class DefaultSettings:
         a ``context`` dictionary with additional render data like ``request``
         and either the ``basket`` or ``order`` object.
         """
-        from django.utils.module_loading import import_string
+        default = 'salesman.core.utils.format_price'
+        value = self._setting('SALESMAN_PRICE_FORMATTER', default)
+        return self._callable(value)
 
-        value = self._setting(
-            'SALESMAN_PRICE_FORMATTER', 'salesman.core.utils.format_price'
-        )
-        try:
-            formatter = import_string(value)
-        except ImportError as e:
-            self._error(e)
-        if not callable(formatter):
-            self._error(f"Specified price formatter `{formatter}` is not callable.")
-        return formatter
+    @property
+    def SALESMAN_ADDRESS_VALIDATOR(self) -> callable:
+        """
+        A dotted path to address validator function. Function should accept a string
+        value and return a validated version. Also recieves a ``context`` dictionary
+        with additional validator context data like ``request``, a ``basket`` object
+        and ``address`` type (set to either *shipping* or *billing*).
+        """
+        default = 'salesman.checkout.utils.validate_address'
+        value = self._setting('SALESMAN_ADDRESS_VALIDATOR', default)
+        return self._callable(value)
+
+    @property
+    def SALESMAN_EXTRA_VALIDATOR(self) -> callable:
+        """
+        A dotted path to extra validator function. Function should accept a dict
+        value and return a validated version. Also recieves a ``context`` dictionary
+        with additional validator context data like ``request``, a ``basket`` object
+        and ``basket_item`` in case validation is for bakset item.
+        """
+        default = 'salesman.basket.utils.validate_extra'
+        value = self._setting('SALESMAN_EXTRA_VALIDATOR', default)
+        return self._callable(value)
 
     @property
     def SALESMAN_ADMIN_REGISTER(self) -> bool:
